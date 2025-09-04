390b5200750c0c71e93889f3e65844a83ab3dc7e
Shadman Sohel <shadman.sohel04@gmail.com>
2025-09-04 18:19:55 -0400

    cleaned up spin up logic

--- nodeServer/routers/mainRouter.js
+++ nodeServer/routers/mainRouter.js
@@ -3,10 +3,13 @@ import { createFood, removeFood } from "../controllers/preGame.js"
 import { getFoods } from "../controllers/getFoods.js"
 import { checkAuthorization } from "../middleware/auth.js"
 
-
+const forSpinUp = async (req, res) => {
+    return res.status(200).json({ success: true, message: "yuppp"});
+}
 
 const mainRouter = e.Router()
 
+mainRouter.route("/forSpinUp").get(forSpinUp)
 mainRouter.route('/uploadFood').post(checkAuthorization, createFood)
 mainRouter.route('/deleteRecipe').delete(checkAuthorization, removeFood)
 mainRouter.route('/getRecipes').get(checkAuthorization, getFoods)
